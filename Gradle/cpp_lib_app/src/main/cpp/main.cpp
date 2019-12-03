7cfff859d9868dd443a9a65268026bfccbec788e
Fei Rui <fei@xlnx.xilinx.com>
2019-12-03 12:30:43 -0700

    add more gradle examples; add .gitignore files

---
+++ Gradle/cpp_lib_app/src/main/cpp/main.cpp
@@ -0,0 +1,9 @@
+// main.cpp
+#include "Hello.h"
+int main(int argc, char ** argv)
+{
+    Hello hello ("Pepito");
+    hello.sayHello(10);
+    return 0;
+}
+
---
+++ Gradle/cpp_lib_app_new/hello-lib/src/main/public/hello.h
@@ -0,0 +1,21 @@
+// Hello.h
+#ifdef _MSC_VER
+	#ifdef HELLO_EXPORT
+		#define HELLO_API __declspec(dllexport)
+	#else
+		#define HELLO_API __declspec(dllimport)
+	#endif
+#else
+	#define HELLO_API
+#endif
+
+class HELLO_API Hello
+{
+    private:
+        const char * who;
+    public:
+        Hello(const char * who);
+        void sayHello(unsigned n = 1);
+};
+
+
